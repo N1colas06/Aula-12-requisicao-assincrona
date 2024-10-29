8d76650122e3b9bc5f9b57c440d1f59748d42203
N1colas06 <nicolasdiasrodrigues65@gmail.com>
2024-10-29 20:28:41 -0300

    calculo.php

--- calculo.php
+++ calculo.php
@@ -1,1 +1,24 @@
+<?php
+header('Content-Type: application/json');
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+
+   // Lê o JSON enviado no corpo da requisição
+    $data = json_decode(file_get_contents("php://input"));
+
+    // Verifica se o JSON contém os valores esperados
+    if (isset($data->numero1) && isset($data->numero2)) {
+        // Realiza a soma
+        $soma = $data->numero1 + $data->numero2;
+        
+        // Retorna a resposta em JSON
+        echo json_encode(["soma" => $soma]);
+    } else {
+        // Retorna um erro se os valores não foram enviados corretamente
+        echo json_encode(["erro" => "Valores numero1 e numero2 são necessários."]);
+    }
+} else {
+    echo json_encode(['erro' => 'Método não suportado. Use o POST']);
+}
+?>
